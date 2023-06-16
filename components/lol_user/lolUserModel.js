d708f49555e2695070f2f7e263bf79f4f5eef981
[AI7/2반] 변상기 <tkdrl112@gmail.com>
2023-06-16 23:04:35 +0900

    fix - lolUser 테이블 기본 mannergrade 수정 [변상기] 0616 23:04

--- components/lol_user/lolUserModel.js
+++ components/lol_user/lolUserModel.js
@@ -1,4 +1,4 @@
-const insertLolUser = `INSERT INTO lol_user VALUES (?,0,'bronze',?,?,?,?,?,null,now(),now())`;
+const insertLolUser = `INSERT INTO lol_user VALUES (?,0,'gentle',?,?,?,?,?,null,now(),now())`;
 const selectLolUser = `SELECT * FROM lol_user WHERE lol_id =? AND deleted_at IS NULL`;
 const updateLolUser = `UPDATE lol_user SET level=?, \`rank\`=?, tier=?, wins=?, losses=?, updated_at=now() WHERE lol_id=? AND deleted_at IS NULL`;
 const reportLolUser = `UPDATE lol_user SET report_count =?,manner_grade=?, updated_at=now() WHERE lol_id=? AND deleted_at IS NULL`;
